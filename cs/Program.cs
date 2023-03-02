35b23ab033933c939e0b4fbea6c20f28de79d6af
Takumi Yamashita <64026753+husty530@users.noreply.github.com>
2023-03-02 17:59:21 +0900

    *

--- cs/Program.cs
+++ cs/Program.cs
@@ -7,11 +7,15 @@ Console.WriteLine("Press 'Q' key to exit...");
 
 while (!(Console.KeyAvailable && Console.ReadKey().Key is ConsoleKey.Q))
 {
+    // If you want to write OpenCvSharp's Mat, do the following.
+    // unsafe { memory.TryWrite(new Span<byte>(mat.DataPointer, mat.Width * mat.Height * 3)); }
     if (memory.TryRead(out var buf))
     {
         Console.WriteLine($"Received: {buf.Length} bytes.");
         // If you want to copy to OpenCvSharp's Mat, do the following.
         // unsafe { buf.CopyTo(new Span<byte>(mat.DataPointer, buf.Length)); }
+        // Cv2.ImShow(" ", mat);
+        // Cv2.WaitKey(1);
     }
     else Thread.Sleep(10);
 }
--- py/main.py
+++ py/main.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 import cv2
 from shared_memory_pool import *
 
@@ -6,7 +7,9 @@ if __name__ == "__main__":
   cap = cv2.VideoCapture(os.path.join(os.path.dirname(__file__), "sample.mp4"))
   delay = 1 / cap.get(cv2.CAP_PROP_FPS)
   path = os.path.join(os.path.dirname(__file__), "shared.pool")
-  buf_capacity = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) * cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * 3)
+  w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+  h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+  buf_capacity = w * h * 3
   with SharedMemoryPool(path, buf_capacity, 2) as memory:
     memory.flush()
     while True:
@@ -18,3 +21,8 @@ if __name__ == "__main__":
         print(f"Wrote {len(buf)} bytes.")
       else: 
         break
+      # ret, bin = memory.try_read()
+      # if ret:
+      #   frame = np.frombuffer(bin, dtype=np.uint8).reshape(h, w, 3)
+      #   cv2.imshow(" ", frame)
+      #   cv2.waitKey(1)
